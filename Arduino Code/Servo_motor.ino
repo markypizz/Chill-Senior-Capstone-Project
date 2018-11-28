0aeccac8ec7a2e17c63bb3d9318300e19347d561
jqp5548 <43935161+jqp5548@users.noreply.github.com>
2018-11-28 15:42:48 -0500

    Add files via upload

---
+++ Arduino Code/Servo_motor.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+Servo myservo;
+
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(9600);
+      myservo.attach(9); //pin number
+      
+  myservo.writeMicroseconds(1500); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  myservo.writeMicroseconds(2300); // 2000 or 2300 clockwise
+  delay(2000);
+    myservo.writeMicroseconds(700); // 700 or 1000 counter clockwise
+  delay(2000);
+}
