4db6a24a28aa43af399dcaf89b78bf35475c18ff
Mark <markypizz@gmail.com>
2018-12-06 01:07:49 -0500

    final app. App icon

--- Test2/Test2/ViewController.swift
+++ Test2/Test2/ViewController.swift
@@ -17,33 +17,101 @@ class ViewController: UIViewController, GCDAsyncUdpSocketDelegate, GCDAsyncSocke
     
     @IBOutlet weak var currentTempLabel: UILabel!
     
+    @IBOutlet weak var mainValveLabel: UILabel!
+    
+    @IBOutlet weak var coldValveLabel: UILabel!
+    
+    @IBOutlet weak var showerReadyLabel: UILabel!
+    
+    @IBOutlet weak var alert1Label: UILabel!
+    
+    @IBOutlet weak var alert2Label: UILabel!
+    
+    @IBOutlet weak var alert1Stepper: UIStepper!
+    
+    @IBOutlet weak var alert2Stepper: UIStepper!
+    
+    @IBOutlet weak var confirmButton: UIButton!
+    
+    
+    var alert1Time : Int32 = 5 {
+        didSet {
+            alert1Label.text = "Alert 1: \(alert1Time) minutes"
+            alert1Stepper.value = Double((alert1Time))
+            responses.timer1 = alert1Time
+        }
+        
+    }
+    
+    var alert2Time : Int32 = 3 {
+        didSet {
+            alert2Label.text = "Alert 2: \(alert2Time) minutes"
+            alert2Stepper.value = Double((alert2Time))
+            responses.timer2 = alert2Time
+        }
+    }
+    
     var address = "192.168.4.1"
     var port:UInt16 = 4211
     
     var socket : GCDAsyncUdpSocket!
-    var recvSocket : GCDAsyncSocket!
+    var sendSocket : GCDAsyncUdpSocket!
+    //var recvSocket : GCDAsyncSocket!
     
     let alert = UIAlertController(title: nil, message: "Connecting...", preferredStyle: .alert)
     
     struct stats {
+        var timer1: Int32
+        var timer2: Int32
         var targetTemp: Int16
         var currentTemp: Int16
-        var hotValveClosed: Bool
-        var coldValveClosed: Bool
-        var grayWaterClosed: Bool
+        var servoAngle: UInt8
+        var mainValveClosed: Bool
+        var showerReady: Bool
         
         init() {
             currentTemp = 0
             targetTemp = 0
-            hotValveClosed = false
-            coldValveClosed = false
-            grayWaterClosed = true
+            timer1 = 5
+            timer2 = 3
+            mainValveClosed = false
+            servoAngle = 180
+            showerReady = false
+        }
+    }
+    
+    struct responseStats : Codable {
+        var timer1 : Int32
+        var timer2 : Int32
+        var targetTemp : Int16
+        init() {
+            timer1 = 5
+            timer2 = 3
+            targetTemp = 35
+        }
+        
+        static func archive(w:responseStats) -> Data {
+            var fw = w
+            return Data(bytes: &fw, count: MemoryLayout<responseStats>.stride)
+        }
+        
+        static func unarchive(d:Data) -> responseStats {
+            guard d.count == MemoryLayout<responseStats>.stride else {
+                fatalError("BOOM!")
+            }
+            
+            var w:responseStats?
+            d.withUnsafeBytes({(bytes: UnsafePointer<responseStats>)->Void in
+                w = UnsafePointer<responseStats>(bytes).pointee
+            })
+            return w!
         }
     }
     
     var targetTemp:Int16 = 50 {
         didSet {
             targetTempLabel.text = String(targetTemp) + " C"
+            responses.targetTemp = targetTemp
         }
     }
     
@@ -54,6 +122,8 @@ class ViewController: UIViewController, GCDAsyncUdpSocketDelegate, GCDAsyncSocke
     }
     
     var statuses = stats()
+    var responses = responseStats()
+    var expectingUpdate : Bool = true
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -65,39 +135,31 @@ class ViewController: UIViewController, GCDAsyncUdpSocketDelegate, GCDAsyncSocke
         
         alert.view.addSubview(loadingIndicator)
         
+        //Remove this line to remove waiting screen (for testing)
         present(alert, animated: true, completion: nil)
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let message = "HelloFromPhoneApp".data(using: String.Encoding.utf8)
-        
+        confirmButton.isEnabled = true
         socket = GCDAsyncUdpSocket(delegate: self, delegateQueue: DispatchQueue.main)
-        
+        sendSocket = GCDAsyncUdpSocket(delegate: self, delegateQueue: DispatchQueue.main)
         
         do {
             try socket.bind(toPort: 4210)
             //try socket.acceptOnPort(4210)
             try socket.beginReceiving()
             try socket.enableBroadcast(true)
-            socket.send(message!, toHost: address, port: port, withTimeout: 1000, tag: 0)
-            
+            //socket.send(message!, toHost: address, port: port, withTimeout: 1000, tag: 0)
         } catch {
             print(error)
         }
         
-        // Setup the other socket (used to handle the response from the Harmony hub)
-        recvSocket = GCDAsyncSocket(delegate: self,  delegateQueue:DispatchQueue.main)
-        
         do {
-            
-            // Accept connections on port 57612
-            try recvSocket.accept(onPort: 4210)
-            
+            //try sendSocket.bind(toPort: 4211)
+            try sendSocket.enableBroadcast(true)
+            //try socket.connect()
         } catch {
-            
-            // Handle any errors here
             print(error)
         }
         
@@ -107,6 +169,7 @@ class ViewController: UIViewController, GCDAsyncUdpSocketDelegate, GCDAsyncSocke
         let slider = sender as! UISlider
         
         targetTemp = Int16(slider.value)
+        //sendSocket.send(message!, toHost: "104.38.61.33", port: 4211, withTimeout: 1000, tag: 0)
     }
     
     @IBAction func doneChangingTarget(_ sender: Any) {
@@ -118,7 +181,6 @@ class ViewController: UIViewController, GCDAsyncUdpSocketDelegate, GCDAsyncSocke
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     
     func udpSocket(_ sock: GCDAsyncUdpSocket, didConnectToAddress address: Data) {
         print("didConnectToAddress");
@@ -141,15 +203,83 @@ class ViewController: UIViewController, GCDAsyncUdpSocketDelegate, GCDAsyncSocke
         
         statuses = data.withUnsafeBytes { $0.pointee as stats }
         
-        targetTemp = statuses.targetTemp
         currentTemp = statuses.currentTemp
+        
+        if (statuses.mainValveClosed == true) {
+            mainValveLabel.text = "Main Valve: Closed"
+        } else {
+            mainValveLabel.text = "Main Valve: Open"
+        }
+        
+        coldValveLabel.text = "Cold Valve: \(statuses.servoAngle)º"
+        
+        if (statuses.showerReady == true) {
+            showerReadyLabel.text = "Shower Ready: Yes"
+            alert2Stepper.isEnabled = false
+            alert1Stepper.isEnabled = false
+            sliderControl.isEnabled = false
+            //confirmButton.isEnabled = false
+            
+            //Force update to default vals
+            expectingUpdate = true
+            
+        } else {
+            showerReadyLabel.text = "Shower Ready: No"
+        }
+        
+        if (expectingUpdate) {
+            alert1Time = (statuses.timer1 / 60000)
+            alert2Time = (statuses.timer2 / 60000)
+            targetTemp = statuses.targetTemp
+            sliderControl.value = Float(statuses.targetTemp)
+            //alert1Stepper.value = Double(statuses.timer1)
+            //alert2Stepper.value = Double(statuses.timer2)
+            expectingUpdate = false
+        }
+        
         alert.dismiss(animated: true, completion: nil)
     }
     
-    func socket(_ sock: GCDAsyncSocket, didRead data: Data, withTag tag: Int) {
-        let gotdata: NSString = NSString(data: data, encoding: String.Encoding.utf8.rawValue)!
-        print(gotdata)
-        print("recvd2")
+    func sendUpdate() {
+        //let encode = Enco
+        
+        //let encoder = JSONEncoder()
+        
+        //let data2 = NSData(bytesNoCopy: &responses, length: sizeof(responseStats), freeWhenDone: false)
+        
+        //let data = responseStats.archive(w: responses)
+        
+        //let data = Data(bytes: &responses, count: MemoryLayout.size(ofValue: responseStats.self))
+        var data = Data()
+        //data.append
+        
+        var time1 = responses.timer1 * 60000
+        var time2 = responses.timer2 * 60000
+
+        data.append(Data(bytes: &time1, count: 4))
+        data.append(Data(bytes: &time2, count: 4))
+        data.append(Data(bytes: &responses.targetTemp, count: 2))
+        //data.append(Data(bytes: &responses.targetTemp, count: 2))
+        //data.append(responses.timer2)
+        //data.append(responses.targetTemp)
+        //data.append(responses.targetTemp)
+        //print(data.byt)
+        
+        sendSocket.send(data, toHost: "255.255.255.255", port: 4211, withTimeout: 1000, tag: 0)
+        
+        //expectingUpdate = true
+    }
+    
+    @IBAction func confirmButtonPressed(_ sender: UIButton) {
+        sendUpdate()
+    }
+    
+    @IBAction func stepper1Changed(_ sender: UIStepper) {
+        alert1Time = Int32(sender.value)
+    }
+    
+    @IBAction func stepper2Changed(_ sender: UIStepper) {
+        alert2Time = Int32(sender.value)
     }
 }
 
